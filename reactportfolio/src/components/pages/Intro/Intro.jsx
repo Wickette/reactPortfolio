8b0c73be13973123d7e752e8ed2a8a9d7abe0e98
Wickette <stephany.clark2@gmail.com>
2021-11-16 20:33:49 -0500

    conditional rendering of pages completed, basic styling done(issue with some tailwind properties

--- reactportfolio/src/components/pages/Intro/Intro.jsx
+++ reactportfolio/src/components/pages/Intro/Intro.jsx
@@ -1,11 +1,67 @@
 import React from 'react'
 import "./intro.scss"
+import profilepic from "./stephanie.jpg"
+import Rin from "./Rin.jpg"
+import Kai from "./Kai.jpg"
+import Jynx from "./Jynx.jpg"
 
 const Intro = () => {
     return (
         <div className="intro">
-            
-        </div>
+            <section id="about" class="about">
+                <div class="container aos-init aos-animate" data-aos="fade-up">
+                    <div class="section-title">
+                    <h2>About</h2>
+                    <p>Hello ~  My name is <strong className="name-span">Stephanie Clark</strong></p>
+                    </div>
+
+                    <div class="row">
+                    <div class="col-lg-4">
+                        <img src={profilepic} class="img-fluid" alt=""/>
+                    </div>
+                    <div class="col-lg-8 pt-4 pt-lg-0 content">
+                        <h3>Full Stack Web Developer</h3>
+                        <p class="fst-italic">
+                        I'm a full-stack developer graduated from the Toronto University full-stack Coding Bootcamp.
+                        </p>
+                        <div class="row">
+                        <div class="col-lg-6">
+                            <ul>
+                            <li><i class="bi bi-rounded-right"></i> <strong>LinkedIn:</strong> www.example.com</li>
+                            <li><i class="bi bi-rounded-right"></i> <strong>Phone:</strong> +123 456 7890</li>
+                            <li><i class="bi bi-rounded-right"></i> <strong>City:</strong> Orillia, Ontario, Canada</li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6">
+                            <ul>
+                            <li><i class="bi bi-rounded-right"></i> <strong>GitHub:</strong> www.example.com</li>
+                            <li><i class="bi bi-rounded-right"></i> <strong>Email:</strong> sc.aurora92@gmail.com</li>
+                            <li><i class="bi bi-rounded-right"></i> <strong>Freelance:</strong> Available</li>
+                            </ul>
+                        </div>
+                        </div>
+                        <div class="flex flex-col items-center">
+                                <h4 class="text-center font-bold text-2xl mt-10">Meet my Family</h4>
+                                <div class="flex flex-row">
+                                <figure class="p-6 flex flex-col items-center">
+                                    <img src={Kai} alt="a pic of my cat Kai" class="h-44 w-44 rounded-full" />
+                                    <figcaption class="text-center">The mamas boi - Kai</figcaption>
+                                </figure>
+                                <figure class="p-6 flex flex-col items-center">
+                                    <img src={Jynx} alt="a pic of my cat Jynx" class="h-44 w-44 rounded-full" />
+                                    <figcaption class="text-center">The eldest girl/aloof floof - Jynx</figcaption>
+                                </figure>
+                                <figure class="p-6 flex flex-col items-center">
+                                    <img src={Rin} alt="a pic of my cat Rin" class="h-44 w-44 rounded-full" />
+                                    <figcaption class="text-center">The youngest girl/trouble maker - Rin</figcaption>
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+            </div>
+        </section>
+    </div>
     )
 }
 
--- reactportfolio/src/components/pages/Portfolio/Portfolio.jsx
+++ reactportfolio/src/components/pages/Portfolio/Portfolio.jsx
@@ -4,7 +4,106 @@ import "./portfolio.scss"
 const Portfolio = () => {
     return (
         <div className="portfolio">
-            
+                
+                <section class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 mb-12">
+        <article>
+            <h2 class="text-6xl my-10 font-extrabold text-yellow-100">PROJECTS</h2>
+            <hr class="mb-6 p-1 bg-green-100" />
+            <section class="mt-6 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-x-6 gap-y-8">
+                <div>
+                    <article class="relative w-full h-64 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition duration-300 ease-in-out mb-4" >
+                        <div class="absolute inset-0 bg-black bg-opacity-50 group-hover:opacity-75 transition duration-300 ease-in-out"></div>
+                        <div class="relative w-full h-full px-4 sm:px-6 lg:px-4 flex justify-center items-center">
+                            <h3 class="text-center">
+                                <a class="text-white text-2xl font-bold text-center"
+                                    href="https://wickette.github.io/HappeningWeather/" target="blank">
+                                    <span class="absolute inset-0"></span>
+                                    happeningWeather App
+                                </a>
+                            </h3>
+                        </div>
+                    </article>
+                    <a class="text-yellow-100 mx-36" href="https://github.com/Wickette/HappeningWeather.git"
+                        target="blank">GitHub Code</a>
+                </div>
+                <div>
+                    <article class="relative w-full h-64 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition duration-300 ease-in-out mb-4
+              " >
+                        <div class="absolute inset-0 bg-black bg-opacity-50 group-hover:opacity-75 transition duration-300 ease-in-out"></div>
+                        <div class="relative w-full h-full px-4 sm:px-6 lg:px-4 flex justify-center items-center">
+                            <h3 class="text-center">
+                                <a class="text-white text-2xl font-bold text-center"
+                                    href="https://wickette.github.io/Daily_Work_Scheduler/" target="blank">
+                                    <span class="absolute inset-0"></span>
+                                    Work-Day Scheduler
+                                </a>
+                            </h3>
+                        </div>
+                    </article>
+                    <a class="text-yellow-100 mx-36" href="https://github.com/Wickette/Daily_Work_Scheduler.git"
+                        target="blank">GitHub Code</a>
+                </div>
+                <div>
+                    <article class="relative w-full h-64 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition duration-300 ease-in-out mb-4" >
+                        <div class="absolute inset-0 bg-black bg-opacity-50 group-hover:opacity-75 transition duration-300 ease-in-out"></div>
+                        <div class="relative w-full h-full px-4 sm:px-6 lg:px-4 flex justify-center items-center">
+                            <h3 class="text-center">
+                                <a class="text-white text-2xl font-bold text-center"
+                                    href="https://wickette.github.io/V_A_S_T/" target="blank">
+                                    <span class="absolute inset-0"></span>
+                                    Muscles and Meals <br />(team project)
+                                </a>
+                            </h3>
+                        </div>
+                    </article>
+                    <a class="text-yellow-100 mx-36" href="https://github.com/Wickette/V_A_S_T.git"
+                        target="blank">GitHub Code</a>
+                </div>
+                <div>
+                    <article class="relative h-64 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition duration-300 ease-in-out mb-4" >
+                        <div class="absolute inset-0 bg-black bg-opacity-50 group-hover:opacity-75 transition duration-300 ease-in-out"></div>
+                        <div class="relative w-full h-full px-4 sm:px-6 lg:px-4 flex justify-center items-center">
+                            <h3 class="text-center">
+                                <a class="text-white text-2xl font-bold text-center" href="#" target="">
+                                    <span class="absolute inset-0"></span>
+                                    ~ Coming Soon ~
+                                </a>
+                            </h3>
+                        </div>
+                    </article>
+                    <a class="text-yellow-100 mx-36" href="#" target="">GitHub Code</a>
+                </div>
+                <div>
+                    <article class="relative w-full h-64 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition duration-300 ease-in-out mb-4" >
+                        <div class="absolute inset-0 bg-black bg-opacity-50 group-hover:opacity-75 transition duration-300 ease-in-out"></div>
+                        <div class="relative w-full h-full px-4 sm:px-6 lg:px-4 flex justify-center items-center">
+                            <h3 class="text-center">
+                                <a class="text-white text-2xl font-bold text-center" href="#" target="">
+                                    <span class="absolute inset-0"></span>
+                                    ~ Coming Soon ~
+                                </a>
+                            </h3>
+                        </div>
+                    </article>
+                    <a class="text-yellow-100 mx-36" href="#" target="">GitHub Code</a>
+                </div>
+                <div>
+                    <article class="relative w-full h-64 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition duration-300 ease-in-out mb-4" >
+                        <div class="absolute inset-0 bg-black bg-opacity-50 group-hover:opacity-75 transition duration-300 ease-in-out"></div>
+                        <div class="relative w-full h-full px-4 sm:px-6 lg:px-4 flex justify-center items-center">
+                            <h3 class="text-center">
+                                <a class="text-white text-2xl font-bold text-center" href="#" target="">
+                                    <span class="absolute inset-0"></span>
+                                    ~ Coming Soon ~
+                                </a>
+                            </h3>
+                        </div>
+                    </article>
+                    <a class="text-yellow-100 mx-36" href="#" target="">GitHub Code</a>
+                </div>
+            </section>
+        </article>
+    </section>
         </div>
     )
 }
